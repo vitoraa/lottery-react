7618a8b0d53ac758f91301b0d1ea7c7046fd1db6
vitoraa <vitor.lima@outlook.com>
2022-06-20 17:25:42 -0300

    set a message while wait submit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [players, setPlayers] = useState([]);
   const [balance, setBalance] = useState('');
   const [value, setValue] = useState('');
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
     const loadManager = async() => setManager(await lottery.methods.manager().call());
@@ -29,18 +30,22 @@ function App() {
   const handleSubmit = async(event) => {
     event.preventDefault();
     const accounts = await web3.eth.getAccounts();
+
+    setMessage('Waiting on transaction success...');
+
     await lottery.methods.enter().send({
       from: accounts[0],
       value: web3.utils.toWei(value, 'ether')
     });
+
+    setMessage('You have been entered!');
   };
 
   return (
     <div>
       <h2>Lottery Contract</h2>
-      <p>This contract is managed by {manager}
-         There are currently {players.length} people entered, competing to win {balance} eth.
-      </p>
+      <p>This contract is managed by {manager}</p>
+      <p>There are currently {players.length} people entered, competing to win {balance} eth.</p>
       <hr />
       <form>
         <h4>Want to try your luck?</h4>
@@ -50,6 +55,8 @@ function App() {
         </div>
         <button onClick={handleSubmit}>Enter</button>
       </form>
+      <hr />
+      <h4>{message}</h4>
     </div>
   );
 }
