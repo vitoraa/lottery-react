2d8ef8bb5f6d96b2c06c4b2a8c35486087cf24cc
vitoraa <vitor.lima@outlook.com>
2022-06-20 17:15:19 -0300

    refactor

--- src/App.js
+++ src/App.js
@@ -7,15 +7,12 @@ function App() {
   const [manager, setManager] = useState('');
   const [players, setPlayers] = useState([]);
   const [balance, setBalance] = useState('');
+  const [value, setValue] = useState('');
 
   useEffect(() => {
-    const loadManager = async() => {
-      setManager(await lottery.methods.manager().call());
-    };
+    const loadManager = async() => setManager(await lottery.methods.manager().call());
 
-    const loadPlayers = async() => {
-      setPlayers(await lottery.methods.getPlayers().call());
-    };
+    const loadPlayers = async() => setPlayers(await lottery.methods.getPlayers().call());
 
     const loadBalance = async() => {
       const balance = await web3.eth.getBalance(lottery.options.address);
@@ -27,11 +24,32 @@ function App() {
     loadBalance();
   });
 
+  const handleChange = (event) => setValue(event.target.value);
+
+  const handleSubmit = async(event) => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(value, 'ether')
+    });
+  };
+
   return (
     <div>
       <h2>Lottery Contract</h2>
-      <p>This contract is managed by {manager}</p>
-      <p>There are currently {players.length} people entered, competing to win {balance} eth</p>
+      <p>This contract is managed by {manager}
+         There are currently {players.length} people entered, competing to win {balance} eth.
+      </p>
+      <hr />
+      <form>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of ether to enter</label>
+          <input value={value} onChange={handleChange} />
+        </div>
+        <button onClick={handleSubmit}>Enter</button>
+      </form>
     </div>
   );
 }
